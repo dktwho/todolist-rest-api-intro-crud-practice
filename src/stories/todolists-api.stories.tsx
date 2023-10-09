abd34e2f180dafa4da2cb3fbb20a2bb2de356235
Alexander Korobov <dktwho@mail.ru>
2023-10-09 12:56:54 +0300

    add to GetTodolists in useEffect axios get request

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
+}
+const settings = {
+    withCredentials: true
 }
 
 export const GetTodolists = () => {
@@ -9,7 +13,8 @@ export const GetTodolists = () => {
     useEffect(() => {
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
-
+        axios.get(`https://social-network.samuraijs.com/api/1.1/todo-lists`, settings)
+            .then((res) => setState(res.data))
     }, [])
     return <div>{JSON.stringify(state)}</div>
 }
